f907e9535e65b27a0ac00bad9ecfa981b89776ab
Manuel Alejandro Gomez <manuel.gomezo@udea.edu.co>
2019-08-08 19:03:30 -0500

    First commit project init

---
+++ src/test/java/dominio/integracion/BibliotecarioTest.java
@@ -0,0 +1,127 @@
+package dominio.integracion;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import dominio.Bibliotecario;
+import dominio.Libro;
+import dominio.excepcion.PrestamoException;
+import dominio.repositorio.RepositorioLibro;
+import dominio.repositorio.RepositorioPrestamo;
+import persistencia.sistema.SistemaDePersistencia;
+import testdatabuilder.LibroTestDataBuilder;
+
+public class BibliotecarioTest {
+
+	private static final String CRONICA_DE_UNA_MUERTA_ANUNCIADA = "Cronica de una muerta anunciada";
+	private static final String ISBN_NO_EXISTENTE = "234FABBC";
+	private static final String ISBN_PALINDROMO = "1221";
+	private static final String USUARIO = "Manuel Gomez";
+	
+	private SistemaDePersistencia sistemaPersistencia;
+	
+	private RepositorioLibro repositorioLibros;
+	private RepositorioPrestamo repositorioPrestamo;
+
+	@Before
+	public void setUp() {
+		
+		sistemaPersistencia = new SistemaDePersistencia();
+		
+		repositorioLibros = sistemaPersistencia.obtenerRepositorioLibros();
+		repositorioPrestamo = sistemaPersistencia.obtenerRepositorioPrestamos();
+		
+		sistemaPersistencia.iniciar();
+	}
+	
+
+	@After
+	public void tearDown() {
+		sistemaPersistencia.terminar();
+	}
+
+	@Test
+	public void prestarLibroTest() {
+
+		// arrange
+		Libro libro = new LibroTestDataBuilder().conTitulo(CRONICA_DE_UNA_MUERTA_ANUNCIADA).build();
+		repositorioLibros.agregar(libro);
+		Bibliotecario blibliotecario = new Bibliotecario(repositorioLibros, repositorioPrestamo);
+
+		// act
+		blibliotecario.prestar(libro.getIsbn(), USUARIO);
+
+		// assert
+		Assert.assertTrue(blibliotecario.esPrestado(libro.getIsbn()));
+		Assert.assertNotNull(repositorioPrestamo.obtenerLibroPrestadoPorIsbn(libro.getIsbn()));
+
+	}
+
+	@Test
+	public void prestarNoExistente() {
+
+		// arrange
+		Libro libro = new LibroTestDataBuilder().conIsbn(CRONICA_DE_UNA_MUERTA_ANUNCIADA).build();
+		
+		repositorioLibros.agregar(libro);
+		
+		Bibliotecario blibliotecario = new Bibliotecario(repositorioLibros, repositorioPrestamo);
+
+		// act
+		try {
+			blibliotecario.prestar(ISBN_NO_EXISTENTE, USUARIO);
+			fail();
+			
+		} catch (PrestamoException e) {
+			// assert
+			Assert.assertEquals(Bibliotecario.EL_LIBRO_NO_EXISTE, e.getMessage());
+		}
+	}
+	
+	@Test
+	public void prestarLibroNoDisponibleTest() {
+
+		// arrange
+		Libro libro = new LibroTestDataBuilder().conTitulo(CRONICA_DE_UNA_MUERTA_ANUNCIADA).build();
+		
+		repositorioLibros.agregar(libro);
+		
+		Bibliotecario blibliotecario = new Bibliotecario(repositorioLibros, repositorioPrestamo);
+
+		// act
+		blibliotecario.prestar(libro.getIsbn(), USUARIO);
+		try {
+			blibliotecario.prestar(libro.getIsbn(), USUARIO);
+			fail();
+			
+		} catch (PrestamoException e) {
+			// assert
+			Assert.assertEquals(Bibliotecario.EL_LIBRO_NO_SE_ENCUENTRA_DISPONIBLE, e.getMessage());
+		}
+	}
+	
+	@Test
+	public void prestarLibroPalindromo() {
+
+		// arrange
+		Libro libro = new LibroTestDataBuilder().conIsbn(ISBN_PALINDROMO).build();
+		
+		repositorioLibros.agregar(libro);
+		
+		Bibliotecario blibliotecario = new Bibliotecario(repositorioLibros, repositorioPrestamo);
+
+		// act
+		try {
+			blibliotecario.prestar(libro.getIsbn(), USUARIO);
+			fail();
+			
+		} catch (PrestamoException e) {
+			// assert
+			Assert.assertEquals(Bibliotecario.EL_LIBRO_ES_PALINDROMO, e.getMessage());
+		}
+	}
+}
